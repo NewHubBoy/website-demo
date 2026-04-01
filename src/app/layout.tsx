7b41afc7ad9a465add2c7c0999812b5ec8ee16f8
afei A <57030625+NewHubBoy@users.noreply.github.com>
2026-04-01 23:23:13 +0800

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,21 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "New Arrivals - COS | COS US",
-  description: "Explore the latest additions to our women's collection. Shop timeless, modern pieces designed to last.",
-};
+  title: 'Pixel Frog™',
+  description: 'AI驱动的换装体验 - 像素蛙 ',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' }
+    ]
+  },
+}
 
 export default function RootLayout({
   children,
